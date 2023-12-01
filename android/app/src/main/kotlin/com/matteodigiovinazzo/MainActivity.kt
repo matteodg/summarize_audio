bc2cea7958bc0b0db72102e12688e453615b1a09
Matteo Di Giovinazzo <matteodg@gmail.com>
2023-12-01 23:20:05 +0100

    Initial commit with counter app from very_good cli

---
+++ android/app/src/main/kotlin/com/matteodigiovinazzo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.matteodigiovinazzo.summarizeaudio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:summarizeaudio/app/app.dart';
+import 'package:summarizeaudio/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
